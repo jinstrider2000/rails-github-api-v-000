c7f548be089404396b2dad3b11f55a6e4bac8ae6
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-21 20:11:26 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,16 @@ class SessionsController < ApplicationController
 
   def create
     resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
-      req.body = JSON.encode({
+      req.body = ActiveSupport::JSON.encode({
         client_id: ENV['CLIENT_ID'],
         client_secret: ENV['CLIENT_SECRET'],
         code: params['code'],
-        redirect_uri: CGI.escape(root_path)
+        redirect_uri: CGI.escape(oauth_redirect_url)
       })
     end
+    access_token = ActiveSupport::JSON.decode(resp)["access_token"]
+    session[:token] = access_token
+    redirect root_path
   end
+  
 end
