de86190917b39fed5ce1546ec04bdeac007b4aa7
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-21 20:43:32 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,9 @@
 class RepositoriesController < ApplicationController
   def index
     resp = Faraday.get("https://api.github.com/user") do |req|
-      req.headers = ActiveSupport::JSON.encode({
+      req.headers = {
         Authorization: session[:token]
-      })
+      }
     end
 
     @username = ActiveSupport::JSON.decode(resp.body)["login"]
