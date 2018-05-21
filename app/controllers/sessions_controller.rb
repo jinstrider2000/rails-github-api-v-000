bbbc3ac0cc0b46e3914c63657ba4d17512c22b51
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-21 19:55:29 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
   def create
-    resp = Faraday.
+    resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+      req.body = JSON.encode({
+        client_id: ENV['CLIENT_ID'],
+        client_secret: ENV['CLIENT_SECRET'],
+        code: params['code'],
+        redirect_uri: CGI.escape(root_path)
+      })
+    end
   end
 end
